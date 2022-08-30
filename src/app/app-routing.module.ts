89ff1c0c73d248831bec67433f9b64de80ecfe8d
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-08-30 14:49:19 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { MainComponent } from './components/main/main.component';
 import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
-  {path: '' , component: MainComponent },
+  {path: '' , component: EditComponent },
   {path: 'main' , component: MainComponent},
   {path: 'edit' ,  component: EditComponent  },
   {path: 'test', component: TestComponent}
